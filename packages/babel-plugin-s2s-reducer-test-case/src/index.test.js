ec705e41203be5fda08531b2efa86621e7acbf00
akameco <akameco.t@gmail.com>
2017-10-03 16:41:18 +0900

    Refactor tests

--- packages/babel-plugin-s2s-reducer-test-case/src/index.test.js
+++ packages/babel-plugin-s2s-reducer-test-case/src/index.test.js
@@ -3,28 +3,20 @@ import path from 'path'
 import pluginTester from 'babel-plugin-tester'
 import plugin from '.'
 
-const fromPath = path.resolve(__dirname, '__fixtures__', 'reducer.js')
-const fromWithStatePath = path.resolve(
-  __dirname,
-  '__fixtures__',
-  'with-initial-state.js'
-)
+const getFixturesPath = x => path.resolve(__dirname, '__fixtures__', x)
+const fromPath = getFixturesPath('reducer.js')
+const fromWithStatePath = getFixturesPath('with-initial-state.js')
 
-pluginTester({
-  title: 'default',
-  plugin,
-  snapshot: true,
-  pluginOptions: { from: fromPath },
-  tests: [
-    {
-      title: 'basic',
-      code: `
+const testCases = [
+  {
+    title: 'basic',
+    code: `
 import reducer, {initialState} from './actions'
     `
-    },
-    {
-      title: 'exist test case',
-      code: `
+  },
+  {
+    title: 'exist test case',
+    code: `
 import reducer, {initialState} from './actions'
 import * as actions from './actions'
 
@@ -32,8 +24,15 @@ test('handle INCREMENT', () => {
   expect(actions.sample()).toEqual(null)
 })
     `
-    }
-  ]
+  }
+]
+
+pluginTester({
+  title: 'default',
+  plugin,
+  snapshot: true,
+  pluginOptions: { from: fromPath },
+  tests: testCases
 })
 
 pluginTester({
@@ -41,25 +40,7 @@ pluginTester({
   plugin,
   snapshot: true,
   pluginOptions: { from: fromWithStatePath },
-  tests: [
-    {
-      title: 'basic',
-      code: `
-import reducer, {initialState} from './actions'
-    `
-    },
-    {
-      title: 'exist test case',
-      code: `
-import reducer, {initialState} from './actions'
-import * as actions from './actions'
-
-test('handle INCREMENT', () => {
-  expect(actions.sample()).toEqual(null)
-})
-    `
-    }
-  ]
+  tests: testCases
 })
 
 pluginTester({

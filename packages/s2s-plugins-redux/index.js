3c8211fdee583f85c8d0ac5ca1648dafdef47dcc
akameco <akameco.t@gmail.com>
2017-10-19 10:39:41 +0900

    Add initial-state-plugin to s2s-plugins

--- packages/s2s-plugins-redux/index.js
+++ packages/s2s-plugins-redux/index.js
@@ -8,6 +8,8 @@ const reducerCaseCreaterPlugin = require('babel-plugin-s2s-reducer-case-creater'
 const actionRootPlugin = require('babel-plugin-s2s-action-root').default
 const reducerTestCasePlugin = require('babel-plugin-s2s-reducer-test-case')
   .default
+const initilaStetePlugin = require('babel-plugin-s2s-initial-state-creater')
+  .default
 const stateRootPlugin = require('babel-plugin-s2s-state-root').default
 const reducerRootPlugin = require('babel-plugin-s2s-reducer-root').default
 
@@ -44,6 +46,10 @@ const plugins = [
       actionRootPlugin,
       { input: 'src/**/actionTypes.js', output: rootActionPath },
     ],
+  },
+  {
+    test: /containers\/.+reducer.js/,
+    plugin: [initilaStetePlugin],
   },
   {
     test: /containers\/.+reducer.js/,
